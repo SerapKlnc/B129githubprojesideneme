de6fcc4a7f37ae35ca44df8d259468706a9d3040
Serap Kılınç <serapkilinc1571@gmail.com>
2023-02-26 16:25:49 +0300

    v3

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -2,7 +2,8 @@ public class Deneme {
 
     public static void main(String[] args) {
         System.out.println("merhabalar" );
-        System.out.println("merhabalar dünya" );
+        System.out.println("merhabalar dunya" );
+        System.out.println("merhabalar hayat guzel mi" );
 
 
     }
